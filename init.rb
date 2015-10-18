9add89e8ef61e455308984ce7949c6be2a641fbb
Bilel KEDIDI <bilel.kedidi@gmail.com>
2015-10-18 21:11:58 +0100

    add task2

--- init.rb
+++ init.rb
@@ -7,8 +7,10 @@ Redmine::Plugin.register :redmine_custom_project do
   author_url 'http://github.com/bilel-kedidi'
 
   settings :default => {
-        'cp'  => 'Project type'
-    }, :partial => 'redmine_cp/setting'
+        'cp'        => 'Project type',
+        'pr_stage'  => 'Project stage',
+        'value'     => 'Initiative'
+    }#, :partial => 'redmine_cp/setting'
 
   menu :admin_menu, :auto_select_suggest, { :controller => 'auto_select_suggest', :action => 'index' }, :caption => 'Suggest auto select'
 end
@@ -16,5 +18,6 @@ end
 Rails.application.config.to_prepare do
   class Hooks < Redmine::Hook::ViewListener
     render_on :view_projects_form, :partial=> 'redmine_cp/show_js'
+    render_on :view_welcome_index_left, :partial=> 'redmine_cp/show_initiate_projects'
   end
 end
